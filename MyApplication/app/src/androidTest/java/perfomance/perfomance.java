68e44711f5787cca1d8b2a29c47861216ae25f1e
ljakkob <larissa.jacob01@gmail.com>
2017-05-11 08:45:24 -0300

    Chat Facebook
    loop de conversa pro mensseger do facebook

--- MyApplication/app/src/androidTest/java/perfomance/perfomance.java
+++ MyApplication/app/src/androidTest/java/perfomance/perfomance.java
@@ -43,6 +43,21 @@ public class perfomance extends Instrumentation {
 
             mDevice.pressBack();
             mDevice.pressBack();
+
+    }
+
+    public void chatFace () throws Exception {
+
+        UiObject TypeMessage = mDevice.findObject(new UiSelector().resourceId("com.facebook.orca:id/text_input_bar"));
+        TypeMessage.clickAndWaitForNewWindow(2000);
+        TypeMessage.setText("123");
+
+        UiObject Send = mDevice.findObject(new UiSelector().resourceId("com.facebook.orca:id/composer_send_action_button"));
+        Send.click();
+
+        mDevice.pressBack();
+        mDevice.pressBack();
+
     }
 
     @Test
@@ -51,22 +66,62 @@ public class perfomance extends Instrumentation {
         UiObject Wapp = mDevice.findObject(new UiSelector().text("WhatsApp"));
         Wapp.clickAndWaitForNewWindow();
         int cont = 0;
-        while (cont<2) {
+        while (cont<55) {
             UiObject Grupo1 = mDevice.findObject(new UiSelector().index(2));
             Grupo1.click();
             chat();
+
             UiObject Grupo2 = mDevice.findObject(new UiSelector().index(3));
             Grupo2.click();
             chat();
+
             UiObject Grupo3 = mDevice.findObject(new UiSelector().index(4));
             Grupo3.click();
+            chat();
+
+            UiObject Grupo4 = mDevice.findObject(new UiSelector().index(5));
+            Grupo4.click();
+            chat();
 
             cont++;
         }
 
         mDevice.pressBack();
 
+
     }
+
+
+    public void Face () throws Exception{
+
+        UiObject Fmsg = mDevice.findObject(new UiSelector().text("Messenger"));
+        Fmsg.clickAndWaitForNewWindow();
+
+        int cont = 0;
+        while (cont<40) {
+            UiObject Grupo1 = mDevice.findObject(new UiSelector().index(0));
+            Grupo1.click();
+            chatFace();
+
+            UiObject Grupo2 = mDevice.findObject(new UiSelector().index(1));
+            Grupo2.click();
+            chatFace();
+
+            UiObject Grupo3 = mDevice.findObject(new UiSelector().className("android.view.ViewGroup").index(2));
+            Grupo3.click();
+            chatFace();
+
+            cont++;
+        }
+
+        mDevice.pressBack();
+
+            }
+
+
+
+
+
 }
 
 
